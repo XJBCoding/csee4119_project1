ac8882ffaa2ae377e136766b5e2d0f29ebda92a0
yuanjihuang <yh3059@columbia.edu>
2018-10-11 23:08:10 -0400

    4th commit

--- proxy/proxy.py
+++ proxy/proxy.py
@@ -3,37 +3,53 @@ from socket import socket, AF_INET, SOCK_STREAM
 import time
 
 
-#import sys
-
+import sys
+def proxy_server(server_ip,port,conn,addr,data):
+    try:
+        serverSocket = socket(AF_INET, SOCK_STREAM)
+        serverSocket.bind((server_ip, port))
+        serverSocket.send(data)
+        print("send to" + str(addr) + " with message: " + sentence)
+        while True:
+            response = serverSocket.recv(1024)
+            if len(response) > 0:
+                conn.send(response)
+            else:
+                break
+        serverSocket.close()
+        conn.close()
+    except:
+        serverSocket.close()
+        conn.close()
+        print('error in proxy_server!')
+        sys.exit(1)
 if __name__ == '__main__':
-    #print(sys.argv)
-    #listen_port = sys.argv[1]
-    #fake_ip = sys.argv[2]
-    #server_ip = sys.argv[3]
-    listen_port = "8080"
-    fake_ip = "127.0.0.1"
-    server_ip = "127.0.0.1"
-    proxySocket = socket(AF_INET, SOCK_STREAM)
-    proxySocket.bind(('', int(listen_port)))
-    proxySocket.listen(1)
-    print('The proxy is ready to receive')
+    print(sys.argv)
+    listen_port = sys.argv[1]
+    fake_ip = sys.argv[2]
+    server_ip = sys.argv[3]
+    #listen_port = "8080"
+    #fake_ip = "127.0.0.1"
+    #server_ip = "127.0.0.1"
+    try:
+        proxySocket = socket(AF_INET, SOCK_STREAM)
+        proxySocket.bind(('', int(listen_port)))
+        proxySocket.listen(1)
+        print('The proxy is ready to receive')
+    except:
+        print('unable to initialize proxy!')
+        sys.exit(2)
     while True:
         try:
             clientSocket, addr = proxySocket.accept()
             sentence = clientSocket.recv(1024)
+            print('client detected! data:',sentence)
             # Processing
             # Todo
-            print("receive from: " + str(addr) + " with message: " + sentence)
+            proxy_server(server_ip,8080,clientSocket,addr,sentence)
         except:
-            clientSocket.close()
             proxySocket.close()
-        try:
-            serverSocket = socket(AF_INET, SOCK_STREAM)
-            serverSocket.bind((server_ip, 8080))
-            serverSocket.send(sentence)
-            print("send to" + str(addr) + " with message: " + sentence)
-        except:
-            serverSocket.close()
-            proxySocket.close()
+            print('proxy shut down.')
+            sys.exit(1)
 
 
