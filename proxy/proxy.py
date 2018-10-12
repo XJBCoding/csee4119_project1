f7b834bf6fd9cf27b4f8349b72f594756947356f
yuanjihuang <yh3059@columbia.edu>
2018-10-11 21:02:03 -0400

    Initial commit

---
+++ proxy/proxy.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+from socket import socket, AF_INET, SOCK_STREAM
+import time
+
+
+import sys
+
+if __name__ == '__main__':
+    listen_port = sys.argv[0]
+    fake_ip = sys.argv[1]
+    server_ip = sys.argv[2]
+
+
+    proxySocket = socket(AF_INET, SOCK_STREAM)
+    proxySocket.bind(('', listen_port))
+    proxySocket.listen(1)
+    print('The proxy is ready to receive')
+    while True:
+        try:
+            clientSocket, addr = proxySocket.accept()
+            sentence = clientSocket.recv(1024)
+            # Processing
+            # Todo
+            print("receive from: " + str(addr) + " with message: " + sentence)
+        except:
+            clientSocket.close()
+            proxySocket.close()
+        try:
+            serverSocket = socket(AF_INET, SOCK_STREAM)
+            serverSocket.bind((server_ip, 8080))
+            serverSocket.send(sentence)
+            print("send to" + str(addr) + " with message: " + sentence)
+        except:
+            serverSocket.close()
+            proxySocket.close()
+
+
